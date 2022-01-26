89c6bea7d5e6464e76a8f13dc6bc2c226b27cb84
aquino@work <andres.aquino@gmail.com>
2022-01-26 14:19:10 -0600

    Minor fixes

--- themes/macuarra/macuarra.theme.sh
+++ themes/macuarra/macuarra.theme.sh
@@ -8,18 +8,18 @@
 
 # hostname color
 function get_host() {
-    local defcolor="${bold_red}"
+    local hostcolor="${bold_red}"
 
-    [ -z "${SSH_CLIENT}" ] && defcolor="${bold_purple}"
-    echo "${defcolor}\h${reset_color} "
+    [ -z "${SSH_CLIENT}" ] && hostcolor="${bold_purple}"
+    echo "${hostcolor}\h${reset_color} "
 }
 
 # username color
 function get_user() {
-    local defcolor="${blue}"
+    local usercolor="${blue}"
 
-    [ 0 -eq $(id -u) ] && defcolor="${red}"
-    echo "${defcolor}\u${reset_color} "
+    [ 0 -eq "$(id -u)" ] && usercolor="${red}"
+    echo "${usercolor}\u${reset_color} "
 }
 
 # datetime string in a simple format
@@ -27,24 +27,8 @@ function get_timestamp() {
     echo "${gray}$(date '+%Y%m%d.%H%M')${reset_color}"
 }
 
-# get branch information
-function get_git_status() {
-    # head
-    git rev-parse --short HEAD >| "${BRANCH_HEAD}" 2>/dev/null
-
-    # status
-    [[ ! -s "${BRANCH_HEAD}" ]] && return
-
-    # info
-    git status --branch >| "${BRANCH_INFO}" 2>/dev/null
-}
-
 # branch status
 function get_branch() {
-    local STATUS=""
-    local BRANCH=""
-    local SHA16=""
-
     # head
     git rev-parse --short HEAD >| "${BRANCH_HEAD}" 2>/dev/null
 
@@ -54,41 +38,38 @@ function get_branch() {
     # info
     git status --branch >| "${BRANCH_INFO}" 2>/dev/null
 
-    # git branch ?
-    STATUS="${green}${reset_color}"
-    SHA16=$(cat "${BRANCH_HEAD}")
+    # status & name
+    local SHA16=$(cat "${BRANCH_HEAD}")
+    local STATUS="${green}${reset_color}"
+    local BRANCH=$(sed -n 's/On branch \(\w*\)/\1/p' "${BRANCH_INFO}")
 
-    # branch name?
-    BRANCH=$(sed -n 's/On branch \(\w*\)/\1/p' "${BRANCH_INFO}")
-
-    # pending to commit ?
-    UNTRACKED=$(sed -n 's/\(Untracked\) files/\1/p' "${BRANCH_INFO}")
+    # untraked?
+    local UNTRACKED=$(sed -n 's/\(Untracked\) files/\1/p' "${BRANCH_INFO}")
     [[ -n "${UNTRACKED}" ]] && STATUS="${yellow}${reset_color}"
 
-    # untracked files ?
-    UNSTAGED=$(sed -n 's/\(not staged\) for commit/\1/p' "${BRANCH_INFO}")
+    # not staged?
+    local UNSTAGED=$(sed -n 's/\(not staged\) for commit/\1/p' "${BRANCH_INFO}")
     [[ -n "${UNSTAGED}" ]] && STATUS="${red}${reset_color}"
+
+    # to be committed
+    local TOBECOMMITED=$(sed -n 's/Changes \(to be committed\)/\1/p' "${BRANCH_INFO}")
+    [[ -n "${UNSTAGED}" ]] && STATUS="${purple}${reset_color}"
 
     #
     echo "${reset_color} ${BRANCH} [${cyan}${SHA16}${reset_color}] ${STATUS}"
 }
 
 #
-function prompt() {
+function _omb_theme_PROMPT_COMMAND() {
     local ps_header="${reset_color}@$(get_host)${bold_yellow}\w ${reset_color}"
     local ps_prompt="$(get_timestamp)$(get_branch) $(get_user)\$${reset_color}"
 
     PS1="\n${ps_header}\n${ps_prompt} "
 }
 
-# Set term to 256color mode, if 256color is not supported, colors won't work properly
-if [[ $COLORTERM = gnome-* && $TERM = xterm ]] && infocmp gnome-256color >/dev/null 2>&1; then
-  export TERM=gnome-256color
-elif infocmp xterm-256color >/dev/null 2>&1; then
-  export TERM=xterm-256color
-fi
+#
+export BRANCH_HEAD="${HOME}/.${TERM_SESSION_ID/*-/}.head"
+export BRANCH_INFO="${HOME}/.${TERM_SESSION_ID/*-/}.branch"
 
-export BRANCH_HEAD="/tmp/${TERM_SESSION_ID/*-/}.head"
-export BRANCH_INFO="/tmp/${TERM_SESSION_ID/*-/}.branch"
-
-_omb_util_add_prompt_command prompt
+#
+_omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
